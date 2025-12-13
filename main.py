9a4fad3f4347a18a00ebf2ca3db50b595046c95f
CarlosBusiness-exe <business10carlos@gmail.com>
2025-12-13 13:21:40 -0300

    The post method was created, to create elements

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi import HTTPException #To threat exceptions
 from fastapi import status #Pass the status code to be used on the threat
 
+from models import Product #Using the model that was created
+
 app = FastAPI()
 
 #Creating ditcs of dicts
@@ -27,7 +29,7 @@ async def get_products():
     return products
 
 #Get one product
-@app.get("/products/{id}") #In keys because is a variable
+@app.get("/products/{id}") #In keys because is a variable(python)
 async def get_product(id: int): #Type hints because need be converted
     try: #Try to find and return the product
         return products[id] #Returning the product
@@ -36,8 +38,25 @@ async def get_product(id: int): #Type hints because need be converted
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found.") #What will be returned(error not found with description)
 
 
+#POST METHOD
+#Create a new item
+#Have to be sent the parameters on the body of the request
+@app.post("/products" , status_code=status.HTTP_201_CREATED) #Using status code only to set the right status when create a element
+async def post_product(product: Product): #Python hints of the model type
+    next_id: int = len(products) + 1 #Python hints to convert, get the size of products(last id) + 1
+    products[next_id] = product #Set this new product on the position(on  the key-> next_id:dict)
+    return product
+
+    """Manually validation
+    if product.id not in products:
+        products[product.id] = product
+        return product
+    else: #if it is already on the datas, raise a erro of conflict
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Already have a product with this id: {product.id}")"""
+
 #Starting using only python command
 if __name__ == "__main__":
     import uvicorn
 
+    #Choosing specific configs parameters
     uvicorn.run("main:app", host="0.0.0.0", port=8000, debug=True, reload=True)
--- models.py
+++ models.py
@@ -3,6 +3,6 @@ from pydantic import BaseModel
 
 #Creating a base model using pydantic
 class Product(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = None #Set that id is not necessary be provided when will create a item
     name: str
     price: float
